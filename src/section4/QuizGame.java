4d26363589118d0ad7805e89b81482421c16478f
ArielBaruch <league@WTS20.attlocal.net>
2018-12-28 13:13:14 -0800

    Day 3

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -14,7 +14,7 @@ public class QuizGame {
 		// 3.  Use an if statement to check if their answer is correct
 		if(sentence.equalsIgnoreCase("Lituya Bay")){
 		// 4.  if the user's answer is correct
-		JOptionPane.showMessageDialog(null, "Yes you are correct");
+		JOptionPane.showMessageDialog(null, "You are correct");
 		
 		// -- add one to their score 
 		score++;
@@ -22,8 +22,27 @@ public class QuizGame {
 			JOptionPane.showMessageDialog(null, "Sorry you are wrong");
 		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String sentence1 = JOptionPane.showInputDialog(null, "What is the biggest ride of 2019");
+		if(sentence1.equalsIgnoreCase("VAUGHAN")) {
+			JOptionPane.showMessageDialog(null, "You are correct");
+		score++;
 		
+		}else {
+			JOptionPane.showMessageDialog(null, "Sorry you are wrong");
+		}
 		// 6.  After all the questions have been asked, print the user's score 
+		String sentence2 = JOptionPane.showInputDialog(null, "What is the biggest statue in the world");
 		
+		if(sentence2.equalsIgnoreCase("Spring Temple Buddha")){
+		
+		JOptionPane.showMessageDialog(null, "You are correct");
+		score++;
+	}else {
+		JOptionPane.showMessageDialog(null, "Sorry you are wrong");
+	}
+		
+		
+		
+		System.out.println(score);
 	}
 }
